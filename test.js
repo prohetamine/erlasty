2b2fe6bc78d7da5f38301f54b1b46efc60cafa2b
Stas Prohetamine <prohetamine@gmail.com>
2021-06-17 11:26:19 +0300

    save

--- test.js
+++ test.js
@@ -3,7 +3,12 @@ const express     = require('express')
     , erlasty     = require('./index')
 
 const port = 3000
+const config = {
+  count: 7,
+  interval: 500,
+  timeout: 2000
+}
 
-app.use(erlasty.static(__dirname + '/app/build', port))
+app.use(erlasty.static(__dirname + '/app/build', port, config))
 
 app.listen(port)
